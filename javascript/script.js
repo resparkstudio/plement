46a84b6bf66781c27f71bae80eadb6fff4d16ffa
RytisKac <rytiskacinskis@gmail.com>
2024-06-21 11:14:31 +0300

    changes

--- javascript/script.js
+++ javascript/script.js
@@ -93,7 +93,7 @@ const processLineAnimation = function () {
 	const lastItem = document.querySelector('.process-item.is-last');
 	const lastItemHeight = lastItem.offsetHeight;
 	const animatedLineWrap = document.querySelector('.process-line-wrap');
-	animatedLineWrap.style.height = `calc(100% - ${lastItemHeight}px)`;
+	animatedLineWrap.style.height = `calc(100% - ${lastItemHeight}px - 10px)`;
 
 	gsap.set(animatedLine, { transformOrigin: 'center top' });
 
