0600587b85b9311c0462e01c83ccf2f0cf626560
RytisKac <rytiskacinskis@gmail.com>
2024-06-21 11:12:58 +0300

    changes

--- theme/template-parts/section/section-process.php
+++ theme/template-parts/section/section-process.php
@@ -40,10 +40,10 @@ function step_card( $step, $index ) {
 	</h2>
 	<div class="relative">
 		<div
-			class="process-line-wrap -z-[1] bg-lightGrayBg w-[2px] h-full -ml-[1px]] absolute top-[70px] bottom-0 left-1/2">
+			class="process-line-wrap -z-[1] bg-lightGrayBg w-[2px] h-full -ml-[1px]] absolute top-[70px] bottom-0 left-[26px] lg:left-1/2">
 			<div class="process-line bg-accent w-full h-full"></div>
 		</div>
-		<div class="max-w-[31.9375rem] mx-auto lg:max-w-[62.5rem] lg:w-full space-y-[3.5rem] lg:space-y-0 relative">
+		<div class="max-w-[31.9375rem] lg:mx-auto lg:max-w-[62.5rem] lg:w-full space-y-[3.5rem] lg:space-y-0 relative">
 			<?php
 			$index = 1;
 			foreach ( $process_content['steps'] as $step ) :
