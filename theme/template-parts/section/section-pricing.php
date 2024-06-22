358bb96f536512920c630181094c3a9dd9062160
RytisKac <rytiskacinskis@gmail.com>
2024-06-22 16:40:49 +0300

    fix

--- theme/template-parts/section/section-pricing.php
+++ theme/template-parts/section/section-pricing.php
@@ -408,64 +408,65 @@ function standalone_solution( $standalone_solutions ) {
 			</li>
 		</ul>
 		<div class="swiper-pagination !static mt-6"></div>
-		<li class=" mt-8 inline-flex md:hidden flex-col justify-between w-full p-5 bg-accent text-white rounded-2xl">
-			<div class="mb-6">
-				<div class="text-[2.5rem] font-medium flex">
-					<span class="selected-services-count" x-text="selectedServices.length">0</span>
-					/
-					<span><?php echo count( $standalone_solutions['standalone_list'] ) ?></span>
+		<div class="container">
+
+			<div class=" mt-8 inline-flex md:hidden flex-col justify-between w-full p-5 bg-accent text-white rounded-2xl">
+				<div class="mb-6">
+					<div class="text-[2.5rem] font-medium flex">
+						<span class="selected-services-count" x-text="selectedServices.length">0</span>
+						/
+						<span><?php echo count( $standalone_solutions['standalone_list'] ) ?></span>
+					</div>
+					<span><?php echo esc_html_e( 'Services selected', 'plmt' ) ?></span>
 				</div>
-				<span><?php echo esc_html_e( 'Services selected', 'plmt' ) ?></span>
+				<div>
+					<button @click="modalOpen=true" :value="selectedServices"
+						class="standalone-button w-full justify-center button bg-white text-accent hover:bg-white">
+						<?php esc_html_e( 'Book a Call', 'plmt' ) ?>
+						<svg xmlns='http://www.w3.org/2000/svg' width='10' height='9' fill='none'
+							xmlns:v='https://vecta.io/nano'>
+							<path
+								d='M1.154.667a.67.67 0 0 0 .667.667h5.06l-5.92 5.92c-.062.062-.111.135-.144.216s-.051.167-.051.254.017.174.051.254.082.154.144.216.135.111.216.144.167.051.254.051.174-.017.254-.051.154-.082.216-.144l5.92-5.92v5.06A.67.67 0 0 0 8.487 8a.67.67 0 0 0 .667-.667V.667A.67.67 0 0 0 8.487 0H1.821a.67.67 0 0 0-.667.667z'
+								fill='#ED5623' />
+						</svg>
+					</button>
+				</div>
 			</div>
-			<div>
-				<button @click="modalOpen=true" :value="selectedServices"
-					class="standalone-button w-full justify-center button bg-white text-accent hover:bg-white">
-					<?php esc_html_e( 'Book a Call', 'plmt' ) ?>
-					<svg xmlns='http://www.w3.org/2000/svg' width='10' height='9' fill='none'
-						xmlns:v='https://vecta.io/nano'>
-						<path
-							d='M1.154.667a.67.67 0 0 0 .667.667h5.06l-5.92 5.92c-.062.062-.111.135-.144.216s-.051.167-.051.254.017.174.051.254.082.154.144.216.135.111.216.144.167.051.254.051.174-.017.254-.051.154-.082.216-.144l5.92-5.92v5.06A.67.67 0 0 0 8.487 8a.67.67 0 0 0 .667-.667V.667A.67.67 0 0 0 8.487 0H1.821a.67.67 0 0 0-.667.667z'
-							fill='#ED5623' />
-					</svg>
-				</button>
-			</div>
-	</div>
-	<?php
+		</div>
+		<?php
 }
 
 ?>
 
-<section id="pricing" class="pb-16 lg:pb-36">
-	<div class="relative">
-		<div class=" relative flex flex-col justify-center overflow-hidden">
-			<div
-				x-data="{ isPackages: true, modalOpen: false, selectedPackage: '', termsModalOpen: false, currency: 'usd' }">
-				<input id="selected-package" type="hidden" x-bind:value="selectedPackage">
-				<div class="container">
-					<?php pricing_header( $pricing_content ) ?>
-					<?php currency_switch() ?>
-					<div class="hidden lg:block">
-						<?php isset( $pricing_content['packages'] ) ? package_cards( $pricing_content['packages'] ) : ''; ?>
+	<section id="pricing" class="pb-16 lg:pb-36">
+		<div class="relative">
+			<div class=" relative flex flex-col justify-center overflow-hidden">
+				<div
+					x-data="{ isPackages: true, modalOpen: false, selectedPackage: '', termsModalOpen: false, currency: 'usd' }">
+					<input id="selected-package" type="hidden" x-bind:value="selectedPackage">
+					<div class="container">
+						<?php pricing_header( $pricing_content ) ?>
+						<?php currency_switch() ?>
+						<div class="hidden lg:block">
+							<?php isset( $pricing_content['packages'] ) ? package_cards( $pricing_content['packages'] ) : ''; ?>
+						</div>
 					</div>
-				</div>
-				<div class="lg:hidden">
-					<?php isset( $pricing_content['packages'] ) ? package_cards_mobile( $pricing_content['packages'] ) : ''; ?>
-				</div>
-				<div class="lg:container mx-auto">
-
-					<?php isset( $pricing_content['standalone_solutions'] ) ? standalone_solution( $pricing_content['standalone_solutions'] ) : ''; ?>
-				</div>
-				<div class="container">
-					<?php calendly_modal() ?>
-					<?php terms_modal( $pricing_content['payment_terms'] ) ?>
-					<div class="flex w-full justify-center mt-16">
-
-						<button
-							class="text-lg font-semibold underline hover:text-accent transition-colors duration-300 "
-							@click="termsModalOpen=true"><?php esc_html_e( 'Payment terms' ) ?></button>
+					<div class="lg:hidden">
+						<?php isset( $pricing_content['packages'] ) ? package_cards_mobile( $pricing_content['packages'] ) : ''; ?>
+					</div>
+					<div class="lg:container mx-auto">
+						<?php isset( $pricing_content['standalone_solutions'] ) ? standalone_solution( $pricing_content['standalone_solutions'] ) : ''; ?>
+					</div>
+					<div class="container">
+						<?php calendly_modal() ?>
+						<?php terms_modal( $pricing_content['payment_terms'] ) ?>
+						<div class="flex w-full justify-center mt-16">
+							<button
+								class="text-lg font-semibold underline hover:text-accent transition-colors duration-300 "
+								@click="termsModalOpen=true"><?php esc_html_e( 'Payment terms' ) ?></button>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
