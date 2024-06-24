0b25e62ce809b2d5d10b79e0ff5be0f49c522a93
RytisKac <rytiskacinskis@gmail.com>
2024-06-24 14:19:24 +0300

    fixes

--- theme/header.php
+++ theme/header.php
@@ -36,7 +36,7 @@
 
 	<?php wp_body_open(); ?>
 
-	<div id="page">
+	<div id="page" l>
 		<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'plement' ); ?></a>
 
 		<?php get_template_part( 'template-parts/layout/header', 'content' ); ?>
