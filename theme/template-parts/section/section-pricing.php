960407eced6923b163e62db1e1adaee6ad1c6389
RytisKac <rytiskacinskis@gmail.com>
2024-06-27 19:14:13 +0300

    change

--- theme/template-parts/section/section-pricing.php
+++ theme/template-parts/section/section-pricing.php
@@ -63,7 +63,7 @@ function calendly_modal() {
 				x-cloak>
 				<div x-show="modalOpen" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
 					x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-300"
-					x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" @click="modalOpen=false"
+					x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
 					class="absolute inset-0 w-full h-full bg-black bg-opacity-40"></div>
 				<div x-show="modalOpen" x-trap.inert.noscroll="modalOpen" x-transition:enter="ease-out duration-300"
 					x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
@@ -71,16 +71,16 @@ function calendly_modal() {
 					x-transition:leave="ease-in duration-200"
 					x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
 					x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-					class="max-w-[1350px] relative w-full py-6 bg-lightGrayBg px-7 lg:px-[105px] sm:rounded-lg container">
+					class="w-screen h-screen flex justify-center items-center relative py-6 px-7 lg:px-[105px] sm:rounded-lg">
 					<button @click="modalOpen=false"
-						class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 text-gray-600 rounded-full hover:text-gray-800 hover:bg-gray-50">
-						<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+						class="absolute top-0 right-0 flex items-center justify-center w-10 h-10 mt-5 mr-5 text-lightGray rounded-full hover:text-gray-50">
+						<svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
 							stroke-width="1.5" stroke="currentColor">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 						</svg>
 					</button>
-					<div class="flex flex-col items-start justify-center">
-						<div id="calendlyDiv" class="w-full h-[700px]"></div>
+					<div class="flex flex-col items-start max-w-[1000px] w-full justify-center h-[90%]">
+						<div id="calendlyDiv" class="max-w-[1000px] w-full h-[700px]" @click.away="modalOpen=false"></div>
 					</div>
 				</div>
 			</div>
