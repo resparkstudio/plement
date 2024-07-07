adc63222da8243917ac7b652e51879e0ba4bfe55
RytisKac <rytiskacinskis@gmail.com>
2024-07-08 00:23:46 +0300

    modal refactor

--- theme/inc/template-functions.php
+++ theme/inc/template-functions.php
@@ -271,3 +271,43 @@ function plmt_button_with_arrow( $on_click, $text, $value, $config = array() ) {
 		</button>
 		<?php
 }
+
+function plmt_modal( $modal_id, $contentCallback ) {
+	ob_start();
+	call_user_func( $contentCallback );
+	$content = ob_get_clean();
+
+	?>
+		<div @keydown.escape.window="<?php echo esc_attr( $modal_id ) ?> = false" class="relative z-50 w-auto h-auto">
+			<template x-teleport="body">
+				<div x-show="<?php echo esc_attr( $modal_id ) ?>"
+					class="fixed top-0 left-0 z-[99] flex items-center justify-center w-screen h-screen px-4 lg:px-0"
+					x-cloak>
+					<div x-show="<?php echo esc_attr( $modal_id ) ?>" x-transition:enter="ease-out duration-300"
+						x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+						x-transition:leave="ease-in duration-300" x-transition:leave-start="opacity-100"
+						x-transition:leave-end="opacity-0" @click="<?php echo esc_attr( $modal_id ) ?>=false"
+						class="absolute inset-0 w-full h-full bg-black bg-opacity-40"></div>
+					<div x-show="<?php echo esc_attr( $modal_id ) ?>"
+						x-trap.inert.noscroll="<?php echo esc_attr( $modal_id ) ?>"
+						x-transition:enter="ease-out duration-300"
+						x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+						x-transition:leave="ease-in duration-200"
+						x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+						x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						class="relative px-4 py-14 w-full lg:p-20 bg-lightGrayBg rounded-lg container lg:w-max">
+						<button @click="<?php echo esc_attr( $modal_id ) ?>=false"
+							class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 text-gray-600 rounded-full hover:text-gray-800 hover:bg-gray-50">
+							<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+								stroke-width="1.5" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+							</svg>
+						</button>
+						<?php echo $content ?>
+					</div>
+				</div>
+			</template>
+		</div>
+		<?php
+}
--- theme/template-parts/section/section-pricing.php
+++ theme/template-parts/section/section-pricing.php
@@ -94,41 +94,19 @@ function calendly_modal() {
 function terms_modal( $terms ) {
 	if ( empty( $terms ) )
 		return;
-	?>
-	<div @keydown.escape.window="termsModalOpen = false" class="relative z-50 w-auto h-auto">
-		<template x-teleport="body">
-			<div x-show="termsModalOpen"
-				class="fixed top-0 left-0 z-[99] flex items-center justify-center w-screen h-screen px-4 lg:px-0" x-cloak>
-				<div x-show="termsModalOpen" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
-					x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-300"
-					x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" @click="termsModalOpen=false"
-					class="absolute inset-0 w-full h-full bg-black bg-opacity-40"></div>
-				<div x-show="termsModalOpen" x-trap.inert.noscroll="termsModalOpen"
-					x-transition:enter="ease-out duration-300"
-					x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-					x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-					x-transition:leave="ease-in duration-200"
-					x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-					x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-					class="relative px-4 py-14 w-full lg:p-20 bg-lightGrayBg rounded-lg container lg:w-max">
-					<button @click="termsModalOpen=false"
-						class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 text-gray-600 rounded-full hover:text-gray-800 hover:bg-gray-50">
-						<svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-							stroke-width="1.5" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-						</svg>
-					</button>
-					<div class="flex flex-col justify-center items-center gap-4 mx-auto w-full">
-						<h3><?php echo esc_html( $terms['heading'] ) ?></h3>
-						<p class="max-w-[740px] text-lg font-medium text-center whitespace-pre-line">
-							<?php echo esc_html( $terms['description'] ) ?>
-						</p>
-					</div>
-				</div>
-			</div>
-		</template>
-	</div>
-	<?php
+
+	plmt_modal( "termsModalOpen", function () use ($terms) {
+		?>
+		<div class="flex flex-col justify-center items-center gap-4 mx-auto w-full">
+			<h3><?php echo esc_html( $terms['heading'] ) ?></h3>
+			<p class="max-w-[740px] text-lg font-medium text-center whitespace-pre-line">
+				<?php echo esc_html( $terms['description'] ) ?>
+			</p>
+		</div>
+		<?php
+	} );
+?>
+<?php
 }
 
 function package_cards_mobile( $packages ) {
