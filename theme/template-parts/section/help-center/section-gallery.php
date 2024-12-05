d1d2da7e564e0a152bfb2e2548f6bf5cbb8a8845
RytisKac <rytiskacinskis@gmail.com>
2024-12-05 20:50:48 +0200

    update

--- theme/template-parts/section/help-center/section-gallery.php
+++ theme/template-parts/section/help-center/section-gallery.php
@@ -1,4 +1,5 @@
 <?php
+$gallery_heading = get_field( 'gallery_heading' );
 $gallery_content = get_field( 'gallery' );
 
 if ( ! isset( $gallery_content ) || empty( $gallery_content ) ) {
@@ -8,16 +9,50 @@ if ( ! isset( $gallery_content ) || empty( $gallery_content ) ) {
 
 <section id="gallery" class="container py-20 lg:py-[7.5rem]">
 	<h2 class="text-h4Bold text-center mb-10 lg:text-left lg:text-h2">
-		<?php esc_html_e( $gallery_content['heading'] ) ?>
+		<?php esc_html_e( $gallery_heading ) ?>
 	</h2>
 
 	<div class="swiper help-center-gallery">
 		<div class="swiper-wrapper">
-			<?php foreach ( $gallery_content['images'] as $image ) : ?>
-				<div class="swiper-slide md:aspect-square md:!w-[26.25rem]">
-					<img class="w-full h-full object-cover" src="<?php echo esc_url( $image['url'] ) ?>"
-						alt="<?php echo esc_attr( $image['alt'] ) ?>">
-				</div>
+			<?php foreach ( $gallery_content as $item ) : ?>
+				<?php if ( ! empty( $item['link'] ) ) : ?>
+					<div class="swiper-slide md:aspect-square md:!w-[26.25rem] bg-black group">
+						<a href="<?php echo esc_url( $item['link']['url'] ) ?>" class="w-full h-full  ">
+							<img class="w-full h-full object-cover group-hover:opacity-60 transition-opacity duration-300 ease-in-out"
+								src="<?php echo esc_url( $item['image']['url'] ) ?>"
+								alt="<?php echo esc_attr( $item['image']['alt'] ) ?>">
+							<div
+								class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex items-center w-[22.25rem] py-4 px-6 bg-white justify-between text-accent opacity-0 group-hover:opacity-100 transition-opacity duration-100">
+								<span><?php esc_html_e( 'Go to full page' ) ?></span>
+								<div class="z-1 flex justify-center items-center relative overflow-hidden ">
+									<div
+										class="justify-center items-center w-[1.125rem] h-[1.125rem] transition-transform duration-300 absolute translate-x-[-100%] translate-y-[100%] ">
+										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+											aria-hidden="true" role="img" class="iconify iconify--ic" width=" 100%"
+											height=" 100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+											<path fill="currentColor" d="M6 6v2h8.59L5 17.59L6.41 19L16 9.41V18h2V6z"></path>
+										</svg>
+									</div>
+									<div
+										class="justify-center items-center w-[1.125rem] h-[1.125rem] transition-transform duration-300  ">
+										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+											aria-hidden="true" role="img" class="iconify iconify--ic" width=" 100%"
+											height=" 100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+											<path fill="currentColor" d="M6 6v2h8.59L5 17.59L6.41 19L16 9.41V18h2V6z"></path>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				<?php else : ?>
+					<div class="swiper-slide md:aspect-square md:!w-[26.25rem]">
+
+						<img class="w-full h-full object-cover" src="<?php echo esc_url( $item['image']['url'] ) ?>"
+							alt="<?php echo esc_attr( $item['image']['alt'] ) ?>">
+					</div>
+
+				<?php endif; ?>
 			<?php endforeach; ?>
 		</div>
 		<div class="flex justify-center gap-4 mt-10">
