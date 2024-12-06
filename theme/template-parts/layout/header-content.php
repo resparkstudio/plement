21fa8ccb5d4dd9d909657c90ec0c38c4e702fb6f
RytisKac <rytiskacinskis@gmail.com>
2024-12-06 20:38:43 +0200

    update

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -43,30 +43,30 @@
 					$is_contact_us = isset( $item['is_contact_us'] ) && $item['is_contact_us'];
 					$has_children  = isset( $item['children'] ) && count( $item['children'] );
 					?>
-					<li x-data="{open: false}" @mouseover='open = true' @mouseover.away="open = false"
-						class="border-r-textSecondary h-full w-full border-r font-semibold">
-						<a href="<?php echo esc_url( $item['url'] ); ?>"
-							class="group flex items-center justify-center h-full w-full text-bodyRegular group  transition-colors duration-300 <?php echo $is_contact_us ? 'hover:bg-accent text-accent !text-bodyBold gap-2' : '' ?> <?php echo $has_children || $is_contact_us ? 'scroll-to hover:bg-accent' : 'hover:bg-[#CBCBCC]' ?>">
-							<span class=" <?php echo $has_children || $is_contact_us ? 'group-hover:text-white' : '' ?>">
-								<?php echo esc_html( $item['title'] ); ?>
-							</span>
+					<li x-data="{open: false}" class="border-r-textSecondary h-full w-full border-r font-semibold">
+						<div class="h-full" @mouseover='open = true' @mouseover.away="open = false">
+							<a href="<?php echo esc_url( $item['url'] ); ?>"
+								class="group flex items-center justify-center h-full w-full text-bodyRegular group  transition-colors duration-300 <?php echo $is_contact_us ? 'hover:bg-accent text-accent !text-bodyBold gap-2' : '' ?> <?php echo $has_children || $is_contact_us ? 'scroll-to hover:bg-accent' : 'hover:bg-[#CBCBCC]' ?>">
+								<span
+									class=" <?php echo $has_children || $is_contact_us ? 'group-hover:text-white' : '' ?>">
+									<?php echo esc_html( $item['title'] ); ?>
+								</span>
+								<?php if ( $has_children ) : ?>
+									<svg :class="open && 'rotate-180 text-white'" width="17" height="16" viewBox="0 0 17 16"
+										fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M14.295 5.80865C14.2571 5.71729 14.193 5.6392 14.1108 5.58426C14.0286 5.52932 13.9319 5.5 13.833 5.5H3.83301C3.73412 5.5 3.63745 5.52932 3.55522 5.58427C3.473 5.63921 3.40891 5.7173 3.37107 5.80866C3.33322 5.90003 3.32332 6.00056 3.34262 6.09755C3.36191 6.19455 3.40953 6.28364 3.47946 6.35356L8.47946 11.3535C8.52589 11.4 8.58101 11.4368 8.64167 11.4619C8.70233 11.4871 8.76735 11.5 8.83301 11.5C8.89867 11.5 8.96369 11.4871 9.02435 11.4619C9.08501 11.4368 9.14013 11.4 9.18656 11.3535L14.1866 6.35356C14.2565 6.28364 14.3041 6.19454 14.3234 6.09755C14.3427 6.00056 14.3328 5.90002 14.295 5.80865Z"
+											fill="currentColor" />
+									</svg>
+								<?php endif; ?>
+								<?php if ( $is_contact_us ) : ?>
+									<?php plmt_arrow(); ?>
+								<?php endif; ?>
+							</a>
 							<?php if ( $has_children ) : ?>
-								<svg :class="open && 'rotate-180 text-white'" width="17" height="16" viewBox="0 0 17 16"
-									fill="none" xmlns="http://www.w3.org/2000/svg">
-									<path
-										d="M14.295 5.80865C14.2571 5.71729 14.193 5.6392 14.1108 5.58426C14.0286 5.52932 13.9319 5.5 13.833 5.5H3.83301C3.73412 5.5 3.63745 5.52932 3.55522 5.58427C3.473 5.63921 3.40891 5.7173 3.37107 5.80866C3.33322 5.90003 3.32332 6.00056 3.34262 6.09755C3.36191 6.19455 3.40953 6.28364 3.47946 6.35356L8.47946 11.3535C8.52589 11.4 8.58101 11.4368 8.64167 11.4619C8.70233 11.4871 8.76735 11.5 8.83301 11.5C8.89867 11.5 8.96369 11.4871 9.02435 11.4619C9.08501 11.4368 9.14013 11.4 9.18656 11.3535L14.1866 6.35356C14.2565 6.28364 14.3041 6.19454 14.3234 6.09755C14.3427 6.00056 14.3328 5.90002 14.295 5.80865Z"
-										fill="currentColor" />
-								</svg>
-							<?php endif; ?>
-							<?php if ( $is_contact_us ) : ?>
-								<?php plmt_arrow(); ?>
-							<?php endif; ?>
-						</a>
-						<?php if ( $has_children ) : ?>
-							<div x-cloak x-show='open'
-								class="absolute bg-[#4B4B4B29] w-full left-0 top-[5rem] min-h-full backdrop-blur-[4px] z-[100]">
-								<ul x-cloak
-									class='border-t border-t-textSecondary px-[4.125rem] grid grid-cols-3 top-0 bg-white z-100 absolute left-1/2 -translate-x-1/2  w-full focus:outline-none'
+
+								<ul x-cloak x-show='open'
+									class='border-t border-t-textSecondary px-[4.125rem] grid grid-cols-3 top-[5rem] bg-white z-[1000] absolute left-1/2 -translate-x-1/2  w-full focus:outline-none'
 									role='menu' aria-orientation='vertical' tabindex='-1'
 									x-transition:enter="transition-opacity duration-200" x-transition:enter-start="opacity-0"
 									x-transition:enter-end="opacity-100" x-transition:leave="transition duration-200"
@@ -87,8 +87,11 @@
 										</li>
 									<?php endforeach; ?>
 								</ul>
-							</div>
-						<?php endif; ?>
+							<?php endif; ?>
+						</div>
+						<div x-cloak x-show='open'
+							class="absolute bg-[#4B4B4B29] w-full left-0 top-[5rem] min-h-full backdrop-blur-[4px] z-[100]">
+						</div>
 					</li>
 				<?php endforeach; ?>
 			</ul>
