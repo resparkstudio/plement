d1eff51e9d907beb676c1dfb9d1b34e577c113f5
RytisKac <rytiskacinskis@gmail.com>
2025-07-24 02:03:23 +0300

    fixes

--- theme/template-parts/section/zendesk/section-hero.php
+++ theme/template-parts/section/zendesk/section-hero.php
@@ -10,10 +10,9 @@ if ( ! isset( $hero_content ) || empty( $hero_content ) ) {
 <section id='hero' class='mb-6  lg:mb-20 flex flex-col bg-mainBlack text-white relative '>
 	<div
 		class="pt-6 lg:py-[4.25rem] lg:container mx-auto lg:min-h-[calc(100vh-5rem)] flex flex-col gap-10 items-center lg:flex-row lg:justify-between lg:gap-14">
-		<div class='lg:w-1/2 hero-content lg:max-w-2xl px-5 lg:px-0'>
+		<div class='lg:w-1/2 hero-content px-5 lg:px-0'>
 			<?php if ( $hero_content['heading'] ) : ?>
-				<h1
-					class='text-[3rem] font-semibold lg:text-[5.625rem] lg:font-medium leading-[100%] max-w-[33.125rem] w-full mb-6'>
+				<h1 class='text-[3rem] font-semibold lg:text-[5.625rem] lg:font-medium leading-[100%] w-full mb-6'>
 					<?php echo $hero_content['heading'] ?>
 				</h1>
 			<?php endif; ?>
@@ -21,7 +20,7 @@ if ( ! isset( $hero_content ) || empty( $hero_content ) ) {
 			<div class='flex flex-wrap'>
 				<?php if ( $hero_content['primary_button'] ) : ?>
 					<?php plmt_button( esc_url( $hero_content['primary_button']['url'] ), esc_attr( $hero_content['primary_button']['title'] ), array(
-						'classes' => 'lg:w-[12.5rem] w-full py-3 lg:w-full justify-center lg:w-auto lg:py-5'
+						'classes' => 'lg:w-[12.5rem] w-full py-3 justify-center lg:w-auto lg:py-5'
 					) ) ?>
 				<?php endif; ?>
 				<?php if ( $hero_content['secondary_button'] ) : ?>
