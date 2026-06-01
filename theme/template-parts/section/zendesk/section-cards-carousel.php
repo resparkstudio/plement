5bd79c544027f54be5ef57e5e407359731da8f90
RytisKac <rytiskacinskis@gmail.com>
2026-06-01 20:29:39 +0300

    update

--- theme/template-parts/section/zendesk/section-cards-carousel.php
+++ theme/template-parts/section/zendesk/section-cards-carousel.php
@@ -73,7 +73,7 @@ function render_card($card)
 		</div>
 	<?php endif; ?>
 	<div
-		class="max-w-[90rem] mx-auto hidden lg:block relative after:content-[''] after:absolute after:top-0 after:right-0 after:bottom-0 before:z-10 after:z-10 after:block after:bg-gradientRight  before:content-[''] before:absolute before:top-0 before:left-0  before:bottom-0 before:block before:bg-gradientLeft before:w-14 after:w-14">
+		class="max-w-[120rem] mx-auto hidden lg:block relative after:content-[''] after:absolute after:top-0 after:right-0 after:bottom-0 before:z-10 after:z-10 after:block after:bg-gradientRight  before:content-[''] before:absolute before:top-0 before:left-0  before:bottom-0 before:block before:bg-gradientLeft before:w-14 after:w-14">
 
 		<?php if ($has_top_row): ?>
 			<div class="mb-4 swiper marquee-slider">
