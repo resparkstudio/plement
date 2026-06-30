29440e65d49744bbc310b76a57376e6a13e57d20
RytisKac <rytiskacinskis@gmail.com>
2026-06-30 13:12:15 +0300

    update

--- theme/template-parts/section/about/section-hero.php
+++ theme/template-parts/section/about/section-hero.php
@@ -1,7 +1,7 @@
 <?php
-$hero_content = get_field( 'hero' );
+$hero_content = get_field('hero');
 
-if ( ! isset( $hero_content ) || empty( $hero_content ) ) {
+if (!isset($hero_content) || empty($hero_content)) {
 	return;
 }
 
@@ -11,23 +11,23 @@ if ( ! isset( $hero_content ) || empty( $hero_content ) ) {
 	<div class='flex flex-col gap-12 items-center lg:flex-row lg:gap-[2.25rem]'>
 		<div class='lg:w-1/2 hero-content lg:max-w-[43.5rem]'>
 			<h1 class='text-h1Mobile max-w-[33.125rem] w-full lg:text-h2 mb-6 lg:pl-10'>
-				<?php echo esc_html( $hero_content['heading'] ) ?>
+				<?php echo esc_html($hero_content['heading']) ?>
 			</h1>
 			<div class="border-l-2 pl-4 lg:pl-10 border-l-accent text-title">
 				<?php echo $hero_content['description'] ?>
 			</div>
 			<div class='mt-6 flex justify-between items-center lg:pl-10'>
 				<div class='flex gap-8 flex-wrap'>
-					<?php foreach ( $hero_content['icons'] as $icon ) : ?>
-						<img src='<?php echo esc_url( $icon['url'] ) ?>' alt='<?php echo esc_attr( $icon['alt'] ) ?>'
-							class='h-[3rem]'>
+					<?php foreach ($hero_content['icons'] as $index => $icon): ?>
+						<img src='<?php echo esc_url($icon['url']) ?>' alt='<?php echo esc_attr($icon['alt']) ?>'
+							class='<?php echo $index === 2 ? 'h-[2.8125rem] md:h-[5rem]' : 'h-[2.125rem] md:h-[3.25rem] '; ?>'>
 					<?php endforeach; ?>
 				</div>
 			</div>
 		</div>
 		<div class="hidden lg:flex w-full justify-center lg:max-w-[34.375rem]">
-			<img class="w-full max-w-[19.375rem]" src='<?php echo esc_url( $hero_content['image']['url'] ) ?>'
-				alt='<?php echo esc_attr( $hero_content['image']['alt'] ) ?>'>
+			<img class="w-full max-w-[19.375rem]" src='<?php echo esc_url($hero_content['image']['url']) ?>'
+				alt='<?php echo esc_attr($hero_content['image']['alt']) ?>'>
 		</div>
 	</div>
 </section>
